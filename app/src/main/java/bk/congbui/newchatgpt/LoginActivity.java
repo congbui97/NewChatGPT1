6e4ae1835dc0a96b35afcb153c74f537f63bde17
torri <meo08081508@gmail.com>
2023-02-16 13:22:10 +0700

    commit all FirebaseDatabase

--- app/src/main/java/bk/congbui/newchatgpt/LoginActivity.java
+++ app/src/main/java/bk/congbui/newchatgpt/LoginActivity.java
@@ -3,12 +3,53 @@ package bk.congbui.newchatgpt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText edtUsername,edtPassword,edtRePassword;
+    private Button btnLoginOrSignup;
+    private TextView tvSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        unit();
+        init();
+
+        setOnClick();
+    }
+
+    private void setOnClick() {
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edtRePassword.setVisibility(View.VISIBLE);
+                tvSignUp.setVisibility(View.GONE);
+            }
+        });
+
+    }
+
+    private void unit() {
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+        edtRePassword = findViewById(R.id.edtRepassword);
+        btnLoginOrSignup = findViewById(R.id.btnLoginOrSignup);
+        tvSignUp = findViewById(R.id.tvSignup);
+    }
+
+    private void init(){
+        edtRePassword.setVisibility(View.GONE);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        edtRePassword.setVisibility(View.GONE);
+        tvSignUp.setVisibility(View.VISIBLE);
     }
 }
